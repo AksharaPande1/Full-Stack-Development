44652d8ebdbac57e8ffa4fd2c0760491f34ed5b2
PriyanshuDandriyal <98729911+PriyanshuDandriyal@users.noreply.github.com>
2022-01-31 11:41:12 +0530

    Add files via upload

---
+++ MCA3C/priyanshudandriyal_20712158/Script.js
@@ -0,0 +1,26 @@
+(function(angular) { 'use strict';
+angular.module('ngRepeat', ['ngAnimate']).controller('repeatController', function($scope) { var friends = [
+{name:'Sardul', age:21},
+{name:'Ankit', age:22},
+];
+
+$scope.removeFirst = function() {
+$scope.friends.shift();
+};
+
+$scope.updateAge = function() {
+$scope.friends.forEach(function(el) { el.age = el.age + 5;
+});
+};
+
+$scope.copy = function() {
+$scope.friends = angular.copy($scope.friends);
+};
+
+$scope.reset = function() {
+$scope.friends = angular.copy(friends);
+};
+
+$scope.reset();
+});
+})(window.angular);
