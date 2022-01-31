f4462db5e30ea5b112aa785eb4ceefa69dca920e
TanyaGup <85734415+TanyaGup@users.noreply.github.com>
2022-01-31 11:14:28 +0530

    Update webques2.js

--- MCA3B/TanyaGupta_2001157/webques2.js
+++ MCA3B/TanyaGupta_2001157/webques2.js
@@ -1,6 +1,6 @@
 const http=require("http");
 const server = http.createServer((req,res) => {
-    res.end("SUCCESS");
+    res.end("SUCCESS FROM SERVER");
 });
 server.listen(8001,"127.0.0.1",() => {
     console.log("PORT NUMBER IS 8002");
